e5f71ee0e87fad6481b433c4630aa93becc47445
wendrikycruz <wendriky.cruz@outlook.com>
2022-08-06 18:48:18 -0300

    criando projeto

---
+++ api.reativa/src/test/java/com/wendriky/api/reativa/ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.wendriky.api.reativa;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
